28f1c4215301b3e7cfd42f6876d729266db72e09
Jefferson Guirra <jeffersonguirra8@gmail.com>
2025-10-26 19:55:36 -0300

    feat: create feat container

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 
 import styles from "./page.module.css";
+import {HeaderContainer} from '../components'
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-
+        <HeaderContainer />
       </main>
     </div>
   );
---
+++ components/header/container/HeaderContainer.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css'
+
+export const HeaderContainer = () => {
+  return (
+    <header className={styles.header}>
+      <h1>
+        header
+      </h1>
+
+    </header>
+  )
+}
