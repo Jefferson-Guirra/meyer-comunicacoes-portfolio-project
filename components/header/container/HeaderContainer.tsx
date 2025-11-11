703f194e26da80038cc6182070e1dab9ab754d5d
Jefferson Guirra <jeffersonguirra8@gmail.com>
2025-11-10 22:14:47 -0300

    refactor: resize HeaderContainer component

--- components/header/container/HeaderContainer.tsx
+++ components/header/container/HeaderContainer.tsx
@@ -1,11 +1,17 @@
 import styles from './styles.module.css'
 import {Navigation} from '../navigation/Navigation'
+import { RiSpeakAiLine } from "react-icons/ri";
 
 export const HeaderContainer = () => {
   return (
     <header className={styles.header}>
-      <p>Meyer Comunicações</p>
-      <Navigation />
+      <article className={styles.box}>
+        <article className={ styles.title}>
+          < RiSpeakAiLine color='#DEDEDF' size={25}/>
+          <p>Meyer Comunicações</p>
+        </article>
+        <Navigation />
+      </article>
     </header>
   )
 }
