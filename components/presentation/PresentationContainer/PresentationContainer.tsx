d67cde70cb7ee5ce334ddd283c3505db3e950afa
Jefferson Guirra <jeffersonguirra8@gmail.com>
2025-11-11 23:44:43 -0300

    refactor: ensure image have correct size

--- components/presentation/PresentationContainer/PresentationContainer.tsx
+++ components/presentation/PresentationContainer/PresentationContainer.tsx
@@ -4,19 +4,20 @@ import Image from 'next/image'
 export const PresentationContainer = () => {
   return (
     <section className={ styles.container}>
-      <article className={ styles.title}>
-        <h1>Seu crescimento</h1>
-        <h1> exige uma equipe</h1>
-        <h1>de qualidade!</h1>
-        <div className={styles.subtitle}>
-          <h1>WE GROW TOGETHER</h1>
-        </div>
-        <Image src={'/fundo.png'}
-          alt='fundo' 
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
+      <article className={styles.box}>
+        <article className={ styles.title}>
+          <h1>Seu crescimento</h1>
+          <h1> exige uma equipe</h1>
+          <h1>de qualidade!</h1>
+          <div className={styles.subtitle}>
+            <h1>WE GROW TOGETHER</h1>
+          </div>
+          <Image src={'/fundo.png'}
+            alt='fundo' 
+            width={1920} 
+            height={1080} 
+          />
+        </article>
       </article>
 
     </section>
